0f43ddc5c9e890573629efd9eda51ce1375d32dd
myfriendpaul <pgonzalez069@gmail.com>
2021-09-10 15:14:10 -0700

    Authentication testing

--- models/user.js
+++ models/user.js
@@ -3,11 +3,7 @@ const Schema = mongoose.Schema;
 
 const User = new Schema(
   {
-    firstname: {
-      type: String,
-      required: true,
-    },
-    lastname: {
+    username: {
       type: String,
       required: true,
     },
