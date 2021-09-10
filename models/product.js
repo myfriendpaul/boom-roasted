cee13bd683d0eed05e807d6b1895e0ec8720a6dd
alexvallcorba <alexgvallcorba@gmail.com>
2021-09-10 14:01:33 -0400

    Created schema for user and product

---
+++ models/product.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const Product = new Schema
+(
+  {
+    name: { 
+      type: String, 
+      required: true 
+      },
+    description: { 
+      type: String, 
+      required: true 
+      },
+    price: { 
+      type: String, 
+      required: true 
+      },
+    origin: { 
+      type: String, 
+      required: false  
+      },
+    imgURL: { 
+      type: String, 
+      required: true 
+      }
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('products', Product)
