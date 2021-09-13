979b3e66e47fe0c17234968946e7640480cb71a2
laces <sneakerheadsean@gmail.com>
2021-09-13 09:50:44 -0400

    Created the Layout Component
    Created the foundation for the layout of the app.

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css"
+import Layout from "../../components/Layout/Layout"
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Layout>
+      <div>
+      
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
