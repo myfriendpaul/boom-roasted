440511a969a8133eb7d460e582db11b31933faa1
laces <sneakerheadsean@gmail.com>
2021-09-13 10:37:04 -0400

    Update Home.jsx
    Basic layout of the home page.

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -5,17 +5,28 @@ import React from "react";
 const Home = (props) => {
   return (
     <>
-    <Layout user={props.user}>
-      <div className="home"></div>
+      <Layout user={props.user}>
+        <div className="home"></div>
       </Layout>
-      
       <div className="shop-now-btn">
         <button>Shop Now</button>
-        
       </div>
-     
-      </>
-
+      <div className="divider">
+        <h1>Coffee Divider</h1>
+      </div>
+      <div className="most-popular-cards">
+        <h1>
+        Most popular products
+        </h1>
+      </div>
+      <div className="who-we-are">
+        <h1>Who we are</h1>
+        <div className="p-who-we-are">
+          <div className="who-we-are-image"></div>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </>
   );
 };
 
