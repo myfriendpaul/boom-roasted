a655fdd3cb19387f9cc9a5d84f956138b24ac70d
ASaliba0419 <salibacodes@gmail.com>
2021-09-14 11:27:08 -0400

    Corrected 'I am a button' to 'Add to cart'

--- client/src/screens/ProductList/ProductList.jsx
+++ client/src/screens/ProductList/ProductList.jsx
@@ -29,7 +29,7 @@ const ProductList = (props) => {
                   price={product.price}
                   key={index}
                 />
-                <button>I am button</button>
+                <button>Add to cart</button>
               </>
             );
           })}
