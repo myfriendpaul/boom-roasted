2ac82beeffe95778dc286fd4a7741f51aadfb648
myfriendpaul <pgonzalez069@gmail.com>
2021-09-15 11:12:43 -0700

    Popular updated. Links in product list are fixed

--- client/src/components/Popular/Popular.jsx
+++ client/src/components/Popular/Popular.jsx
@@ -1,29 +1,32 @@
 import { Link } from "react-router-dom";
-import Product from "../Product/Product";
 
 const Popular = () => {
-  const [popProducts, setPopProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const allProducts = await getProducts();
-      //Algo that selects 3 random products then set popProducts to that, will be array of objects
-      setPopProducts(popProducts);
-    };
-    fetchProducts();
-  }, []);
-
   return (
-    <div className="products">
-      {popProducts.map((product, index) => (
-        <Product
-          id={product._id}
-          name={product.name}
-          imgURL={product.imgURL}
-          price={product.price}
-          key={index}
+    <div>
+      <Link to="/products/614217046e043ffccc8940a1">
+        <h3>Andrew's Cup</h3>
+        <img
+          src="https://i.imgur.com/lgljKKl.png"
+          alt="Cool Cup"
+          height="100px"
         />
-      ))}
+      </Link>
+      <Link to="/products/614217046e043ffccc8940a4">
+        <h3>Black Excellence</h3>
+        <img
+          src="https://i.imgur.com/W0380gz.png"
+          alt="Cool Beans"
+          height="100px"
+        />
+      </Link>
+      <Link to="/products/614217046e043ffccc8940a5">
+        <h3>Nadia's Blend</h3>
+        <img
+          src="https://i.imgur.com/W0380gz.png"
+          alt="Cooler Beans"
+          height="100px"
+        />
+      </Link>
     </div>
   );
 };
--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,18 +1,40 @@
 import "./Product.css";
 import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 
 const Product = (props) => {
   return (
     <>
-      <Link className="productList" to={`/products/${props.id}`}>
-        <img src={props.imgURL} alt={props.name} height="200px" width="200px" />
-
-        <div className="price">{props.price}</div>
-
-        <div className="productName">{props.name}</div>
-      </Link>
+      <Card style={{ width: "20rem" }}>
+        <Card.Img
+          variant="top"
+          href={`/products/${props.id}`}
+          src={props.imgURL}
+        />
+        <Card.Body>
+          <Card.Title>
+            <Link className="productList" to={`/products/${props.id}`}>
+              {props.name}
+            </Link>
+          </Card.Title>
+          <Card.Text>{props.price}</Card.Text>
+          <button>
+            <Link className="productList" to={`/products/${props.id}`}>
+              Add
+            </Link>
+          </button>
+        </Card.Body>
+      </Card>
     </>
   );
 };
 
 export default Product;
+
+//   <Link className="productList" to={`/products/${props.id}`}>
+//   <img src={props.imgURL} alt={props.name} height="200px" width="200px" />
+
+//   <div className="price">{props.price}</div>
+
+//   <div className="productName">{props.name}</div>
+// </Link>
