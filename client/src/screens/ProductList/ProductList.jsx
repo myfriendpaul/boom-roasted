f51c1c73c43f0d8c7d47deffe25944977b4d7cf0
so4inka <kallaurchik@gmail.com>
2021-09-15 11:37:10 -0400

    some styling with bootstrap of the ProductList page

--- client/src/screens/ProductList/ProductList.jsx
+++ client/src/screens/ProductList/ProductList.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import { getProducts } from "../../services/products";
 import Product from "../../components/Product/Product";
+import { Link, Route } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 
 const ProductList = (props) => {
   const [products, setProducts] = useState([]);
@@ -17,24 +19,34 @@ const ProductList = (props) => {
 
   return (
     <>
-      <Layout user={props.user}>
-        <div className="products">
-          {products.map((product, index) => {
-            return (
-              <>
-                <Product
-                  id={product._id}
-                  name={product.name}
-                  imgURL={product.imgURL}
-                  price={product.price}
-                  key={index}
-                />
-                <button>Add to cart</button>
-              </>
-            );
-          })}
-        </div>
-      </Layout>
+      {/* <Layout user={props.user}> */}
+      {/* <div className="products"> */}
+      <Layout></Layout>
+      <div className="cards-container">
+        {products.map((product, index) => {
+          return (
+            <Card style={{ width: "20rem" }}>
+              {/* <Product */}
+              <Card.Img variant="top" src={product.imgURL} />
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>{product.price}</Card.Text>
+                <Link to={`/products/${products.id}`}>
+                  <Button variant="primary">Add to cart</Button>
+                  {/* id={product._id} */}
+                  {/* name={product.name} */}
+                  {/* imgURL={product.imgURL} */}
+                  {/* price={product.price} */}
+                  {/* key={index} */}
+                  {/* /> */}
+                  {/* <button>Add to cart</button> */}
+                </Link>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+      {/* </Layout> */}
     </>
   );
 };
