acc7e1b3b174ed2afb35b20ee96cb96211ff711f
myfriendpaul <pgonzalez069@gmail.com>
2021-09-17 09:07:04 -0700

    Finishing touches

--- client/src/screens/SignOut/SignOut.jsx
+++ client/src/screens/SignOut/SignOut.jsx
@@ -1,35 +1,29 @@
-import { useEffect } from 'react'
-import { signOut } from '../../services/users'
-import { useHistory } from 'react-router-dom'
-import './SignOut.css'
-import  Layout from '../../components/Layout/Layout'
-
-
+import { useEffect } from "react";
+import { signOut } from "../../services/users";
+import { useHistory } from "react-router-dom";
+import "./SignOut.css";
+import Layout from "../../components/Layout/Layout";
 
 const SignOut = (props) => {
-  const { setUser } = props
-  const history = useHistory()
+  const { setUser } = props;
+  const history = useHistory();
 
   useEffect(() => {
     const signOutUser = async () => {
-      await signOut()
-      setUser(null)
-      history.push('/sign-out')
-    }
-    signOutUser()
-  }, [history, setUser])
+      await signOut();
+      setUser(null);
+      history.push("/sign-out");
+    };
+    signOutUser();
+  }, [history, setUser]);
 
   return (
     <Layout className="mediaSignup" user={props.user}>
-    <div className="signOut">
-      <p id="paragraph" >
-        We’ll miss you!
-        Come back and visit us anytime</p>
-    </div>
+      <div className="signOut">
+        <p id="paragraph">We’ll miss you! Come back and visit us anytime boo</p>
+      </div>
     </Layout>
+  );
+};
 
-  )
-}
-
-
-export default SignOut
+export default SignOut;
