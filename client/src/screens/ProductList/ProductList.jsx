b67e5044a80fe230f4d1737a0a80463cd0288b68
myfriendpaul <pgonzalez069@gmail.com>
2021-09-16 18:02:17 -0700

    fixed

--- client/src/screens/ProductList/ProductList.jsx
+++ client/src/screens/ProductList/ProductList.jsx
@@ -4,9 +4,9 @@ import Layout from "../../components/Layout/Layout";
 import { getProducts } from "../../services/products";
 import Product from "../../components/Product/Product";
 
-// import { Link, Route } from "react-router-dom";
-// import Popular from "../../components/Popular/Popular";
-// import { Card, Button } from "react-bootstrap";
+import { Link, Route } from "react-router-dom";
+import Popular from "../../components/Popular/Popular";
+import { Card, Button } from "react-bootstrap";
 
 const ProductList = (props) => {
   const [products, setProducts] = useState([]);
